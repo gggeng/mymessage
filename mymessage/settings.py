d5eb77ef8a240d08cc25f7b07783728ee1fde275
gggeng <gggeng@163.com>
2015-10-29 11:34:57 +0800

    test commit

--- mymessage/settings.py
+++ mymessage/settings.py
@@ -105,6 +105,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 #TEMPLATE_DIRS = (
 #    os.path.join(BASE_DIR,  'templates'),
 #)
